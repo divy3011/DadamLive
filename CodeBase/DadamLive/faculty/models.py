27090262a5a5917dd49765e0f8806d7cdb10ba9a
Aditya-2001 <aditya.iiita2001@gmail.com>
2021-09-23 01:33:10 +0530

    Database added for quiz part with 2 types of question - mcq and written question

--- CodeBase/DadamLive/faculty/models.py
+++ CodeBase/DadamLive/faculty/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from home.models import UserType
+from django_mysql.models import ListTextField
 
 # Create your models here.
 class Course(models.Model):
@@ -10,3 +12,63 @@ class Course(models.Model):
 
     def __str__(self):
         return self.courseName
+
+class Enrolment(models.Model):
+    course=models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    userType=models.ForeignKey(UserType, on_delete=models.SET_NULL, null=True, blank=True)
+    enrolled_on=models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        return self.course.courseName
+
+class Quiz(models.Model):
+    course=models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    start_date=models.DateTimeField(default=datetime.datetime.now())
+    end_date=models.DateTimeField(default=datetime.datetime.now())
+    created_on=models.DateTimeField(default=datetime.datetime.now())
+    quizHeld=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.course.courseName
+
+class MCQ(models.Model):
+    quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True, blank=True)
+    question=models.CharField(null=True, max_length=500)
+
+    options=ListTextField(base_field=models.CharField(default="", max_length=1000), size=10)
+    #Rearrange the array to make correct answer as first index
+
+    correct_answers=ListTextField(base_field=models.IntegerField(default=0), size=10)
+    note=models.CharField(null=True, max_length=500)
+    max_time_limit_allowed=models.IntegerField(default=600, null=True, blank=True)
+    #In seconds for obvious. Default is 10 minutes
+
+    def __str__(self):
+        return self.quiz.course.courseName
+
+class WrittenQuestion(models.Model):
+    quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True, blank=True)
+    question=models.CharField(null=True, max_length=500)
+    note=models.CharField(null=True, max_length=500)
+    max_time_limit_allowed=models.IntegerField(default=600, null=True, blank=True)
+    #In seconds for obvious. Default is 10 minutes
+
+    def __str__(self):
+        return self.quiz.course.courseName
+    
+class Submission(models.Model):
+    quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True, blank=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    answers_marked=ListTextField(base_field=models.CharField(default="", max_length=10000), size=100)
+    question_id=ListTextField(base_field=models.CharField(default="", max_length=100), size=100)
+
+    question_type=ListTextField(base_field=models.CharField(default="", max_length=10), size=100)
+    #1 - MCQ
+    #2 - Written Question
+
+    score=models.CharField(null=True, max_length=20)
+
+    def __str__(self):
+        return self.quiz.course.courseName
