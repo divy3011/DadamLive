c69e693bc1789af74b7174740216d4dcea8a33f0
divy3011 <divyias3011@gmail.com>
2021-10-13 22:17:36 +0530

    pagination added for the quiz page, question number to be changed for change in question

--- CodeBase/MCQQuiz/js/script.js
+++ CodeBase/MCQQuiz/js/script.js
@@ -7,3 +7,17 @@ async function start(){
     let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     video.srcObject = stream;
 }
+$(document).ready(function(){
+    $('.next').click(function(){
+        $('.pagination').find('.pagenumber.active').next().attr("hidden", false);
+        $('.pagination').find('.pagenumber.active').next().addClass('active');
+        $('.pagination').find('.pagenumber.active').prev().attr("hidden", true);
+        $('.pagination').find('.pagenumber.active').prev().removeClass('active');
+    })
+    $('.prev').click(function(){
+        $('.pagination').find('.pagenumber.active').prev().attr("hidden", false);
+        $('.pagination').find('.pagenumber.active').prev().addClass('active');
+        $('.pagination').find('.pagenumber.active').next().attr("hidden", true);
+        $('.pagination').find('.pagenumber.active').next().removeClass('active');
+    })
+})
