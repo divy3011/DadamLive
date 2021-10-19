dd0768b1d6c237938782b66762b373a0d77d7513
divy3011 <divyias3011@gmail.com>
2021-10-19 13:58:15 +0530

    bug in storing proof for tab switch detection fixed

--- CodeBase/DadamLive/static/Student/js/start_quiz.js
+++ CodeBase/DadamLive/static/Student/js/start_quiz.js
@@ -225,7 +225,8 @@ function setWindowsTimeOut(){
     var x=setTimeout(function() {
         window.blur();
         $(window).focus(function() {
-            if(numberOfTimesWindowsTimedOut>=3){
+            if(numberOfTimesWindowsTimedOut>3){
+                numberOfTimesWindowsTimedOut++;
                 connectWithScreenRecorder();
             }
         });
