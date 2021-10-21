8caf8266df5ca82b9b0783d4cf8413ceb840ec86
Aditya-2001 <aditya.iiita2001@gmail.com>
2021-10-21 19:29:22 +0530

    Added the home UI into webserver home page

--- CodeBase/DadamLive/home/urls.py
+++ CodeBase/DadamLive/home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('login/',views.login_request,name="login_request"),
     path('logout/',views.logout_request,name="logout_request"),
+    path('redirect/dashboard/',views.login_redirecter,name="login_redirecter"),
+    
 ]
